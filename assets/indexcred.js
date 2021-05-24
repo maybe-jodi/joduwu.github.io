690f75d2b0fe024ad9aaf13d292e5140c4c09a93
NTBeta <78397696+NTBeta@users.noreply.github.com>
2021-05-24 15:13:23 -0500

    My Chem theme

--- assets/indexcred.js
+++ assets/indexcred.js
@@ -9,3 +9,12 @@ function bowserf() {
   document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
 }
+function mychem() {
+  document.body.style.backgroundImage = "url('function bowserf() {
+  document.body.style.backgroundImage = "url('A2BEC748-B975-4868-A0A9-1E7EE53217CB.webp')";
+  document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
+    var image = document.getElementById('container');
+    image.style.width = '100%';
+    image.style.height = 'auto';
+}
