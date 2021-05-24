342d1cff467d4267e66d5de17a05c7abe652a89e
NTBeta <78397696+NTBeta@users.noreply.github.com>
2021-05-24 15:13:00 -0500

    My Chem theme

--- assets/index.js
+++ assets/index.js
@@ -14,7 +14,7 @@ document.body.style.backgroundSize = "cover";
 }
 function mychem() {
   document.body.style.backgroundImage = "url('function bowserf() {
-  document.body.style.backgroundImage = "url('')";
+  document.body.style.backgroundImage = "url('A2BEC748-B975-4868-A0A9-1E7EE53217CB.webp')";
   document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
     var image = document.getElementById('container');
