c5da2f0ceb47a702c19e5b6ff9fe2f8da48cad86
NTBeta <78397696+NTBeta@users.noreply.github.com>
2021-05-24 20:41:04 -0500

    MCR theme

--- assets/index.js
+++ assets/index.js
@@ -14,7 +14,7 @@ document.body.style.backgroundSize = "cover";
 }
 function mychem() {
   document.body.style.backgroundImage = "url('function bowserf() {
-  document.body.style.backgroundImage = "url('A2BEC748-B975-4868-A0A9-1E7EE53217CB.webp')";
+  document.body.style.backgroundImage = "url('A2E081D7-E622-4241-BC75-C7EE4CC3C19F.jpeg')";
   document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
     var image = document.getElementById('container');
